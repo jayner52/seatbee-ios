59124ea80e64cba6052dbdf6f21b1a8cff2b5fd8
shayancelso <shayan.mirzazadeh@gmail.com>
2026-05-01 23:17:07 +0400

    Initial Commit

---
+++ seatbee/seatbeeApp.swift
@@ -0,0 +1,32 @@
+//
+//  seatbeeApp.swift
+//  seatbee
+//
+//  Created by Shayan Mirzazadeh on 2026-05-01.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct seatbeeApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ seatbeeTests/seatbeeTests.swift
@@ -0,0 +1,19 @@
+//
+//  seatbeeTests.swift
+//  seatbeeTests
+//
+//  Created by Shayan Mirzazadeh on 2026-05-01.
+//
+
+import Testing
+@testable import seatbee
+
+struct seatbeeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
